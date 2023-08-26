dc39ea0a7bef5ccd525c44383028189b2ee9289e
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2023-08-26 19:50:55 +0200

    Add basic navigation support.

--- app/src/main/java/de/selfmade4u/statenotifier/MainActivity.kt
+++ app/src/main/java/de/selfmade4u/statenotifier/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.BackHandler
 import androidx.activity.compose.setContent
+import androidx.annotation.StringRes
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -21,20 +22,30 @@ import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.ModalDrawerSheet
+import androidx.compose.material3.ModalNavigationDrawer
 import androidx.compose.material3.NavigationDrawerItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.navigation.NavDestination.Companion.hierarchy
+import androidx.navigation.NavGraph.Companion.findStartDestination
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
 import de.selfmade4u.statenotifier.ui.theme.StateNotiferTheme
 import kotlinx.coroutines.launch
 
@@ -47,15 +58,24 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+sealed class Screen(val route: String, @StringRes val resourceId: Int) {
+    object Discover : Screen("discover", R.string.discover)
+    object Advertise : Screen("advertise", R.string.advertise)
+}
+
+
 @OptIn(ExperimentalMaterial3Api::class)
 @Preview(showBackground = true)
 @Composable
 fun MainActivityContent() {
+    val navController = rememberNavController()
+
     val drawerState = rememberDrawerState(DrawerValue.Closed)
     val scope = rememberCoroutineScope()
-// icons to mimic drawer destinations
-    val items = listOf(Icons.Default.Favorite, Icons.Default.Face, Icons.Default.Email)
-    val selectedItem = remember { mutableStateOf(items[0]) }
+    val items = listOf(
+        Screen.Discover,
+        Screen.Advertise,
+    )
     BackHandler(enabled = drawerState.isOpen) {
         scope.launch {
             drawerState.close()
@@ -63,60 +83,96 @@ fun MainActivityContent() {
     }
     StateNotiferTheme {
         Scaffold(
-            topBar = {
-                TopAppBar(
-                    title = {
-                        Text(
-                            "StateNotifier",
-                            maxLines = 1,
-                            overflow = TextOverflow.Ellipsis
-                        )
-                    },
-                    navigationIcon = {
-                        IconButton(onClick = { scope.launch { drawerState.open() } }) {
-                            Icon(
-                                imageVector = Icons.Filled.Menu,
-                                contentDescription = "Localized description"
-                            )
+            content = { innerPadding ->
+                ModalNavigationDrawer(
+                    drawerState = drawerState,
+                    drawerContent = {
+                        ModalDrawerSheet {
+                            Spacer(Modifier.height(12.dp))
+                            val navBackStackEntry by navController.currentBackStackEntryAsState()
+                            val currentDestination = navBackStackEntry?.destination
+                            items.forEach { screen ->
+                                NavigationDrawerItem(
+                                    icon = {
+                                        Icon(
+                                            Icons.Filled.Favorite,
+                                            contentDescription = null
+                                        )
+                                    },
+                                    label = { Text(stringResource(screen.resourceId)) },
+                                    selected = currentDestination?.hierarchy?.any { it.route == screen.route } == true,
+                                    onClick = {
+                                        navController.navigate(screen.route) {
+                                            // Pop up to the start destination of the graph to
+                                            // avoid building up a large stack of destinations
+                                            // on the back stack as users select items
+                                            popUpTo(navController.graph.findStartDestination().id) {
+                                                saveState = true
+                                            }
+                                            // Avoid multiple copies of the same destination when
+                                            // reselecting the same item
+                                            launchSingleTop = true
+                                            // Restore state when reselecting a previously selected item
+                                            restoreState = true
+                                        }
+                                        scope.launch { drawerState.close() }
+                                    }
+                                )
+                            }
                         }
                     },
-                )
-            },
-            content = { innerPadding ->
-                Box(Modifier.padding(innerPadding)) {
-                    DismissibleNavigationDrawer(
-                        drawerState = drawerState,
-                        drawerContent = {
-                            DismissibleDrawerSheet {
-                                Spacer(Modifier.height(12.dp))
-                                items.forEach { item ->
-                                    NavigationDrawerItem(
-                                        icon = { Icon(item, contentDescription = null) },
-                                        label = { Text(item.name) },
-                                        selected = item == selectedItem.value,
-                                        onClick = {
-                                            scope.launch { drawerState.close() }
-                                            selectedItem.value = item
-                                        },
-                                        modifier = Modifier.padding(horizontal = 12.dp)
+                    content = {
+                        Column {
+                            TopAppBar(
+                                title = {
+                                    Text(
+                                        "StateNotifier",
+                                        maxLines = 1,
+                                        overflow = TextOverflow.Ellipsis
                                     )
+                                },
+                                navigationIcon = {
+                                    IconButton(onClick = { scope.launch { drawerState.open() } }) {
+                                        Icon(
+                                            imageVector = Icons.Filled.Menu,
+                                            contentDescription = "Localized description"
+                                        )
+                                    }
+                                },
+                            )
+                            NavHost(
+                                navController = navController,
+                                startDestination = Screen.Discover.route,
+                                Modifier.padding(innerPadding)
+                            ) {
+                                composable(Screen.Discover.route) {
+                                    Column(
+                                        modifier = Modifier
+                                            .fillMaxSize()
+                                            .padding(16.dp),
+                                        horizontalAlignment = Alignment.CenterHorizontally
+                                    ) {
+                                        Text(text = if (drawerState.isClosed) ">>> Discover >>>" else "<<< Discover <<<")
+                                        Spacer(Modifier.height(20.dp))
+
+                                    }
+                                }
+                                composable(Screen.Advertise.route) {
+                                    Column(
+                                        modifier = Modifier
+                                            .fillMaxSize()
+                                            .padding(16.dp),
+                                        horizontalAlignment = Alignment.CenterHorizontally
+                                    ) {
+                                        Text(text = if (drawerState.isClosed) ">>> Advertise >>>" else "<<< Advertise <<<")
+                                        Spacer(Modifier.height(20.dp))
+
+                                    }
                                 }
                             }
-                        },
-                        content = {
-                            Column(
-                                modifier = Modifier
-                                    .fillMaxSize()
-                                    .padding(16.dp),
-                                horizontalAlignment = Alignment.CenterHorizontally
-                            ) {
-                                Text(text = if (drawerState.isClosed) ">>> Swipe >>>" else "<<< Swipe <<<")
-                                Spacer(Modifier.height(20.dp))
-
-                            }
                         }
-                    )
-                }
+                    }
+                )
             }
         )
     }
